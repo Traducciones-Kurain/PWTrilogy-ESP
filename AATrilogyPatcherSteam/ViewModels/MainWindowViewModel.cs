6c8c7fb5d62f67ac06d66daafbdf159a907e4fa1
WorstAquaPlayer <tf2worth@hotmail.com>
2021-12-18 06:05:28 -0300

    Add project files.

---
+++ AATrilogyPatcherSteam/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AATrilogyPatcherSteam.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ AATrilogyPatcherSteam/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AATrilogyPatcherSteam.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
