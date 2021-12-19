86c4d1b0b9e8088829bde1c101036e959640046c
WorstAquaPlayer <tf2worth@hotmail.com>
2021-12-19 01:46:17 -0300

    Almacenamiento de arguments en una variable

--- AATrilogyPatcherSteam/Program.cs
+++ AATrilogyPatcherSteam/Program.cs
@@ -7,12 +7,19 @@ namespace AATrilogyPatcherSteam
 {
     class Program
     {
+        public static string[] launchArgs;
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
         public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+            .StartWithClassicDesktopLifetime(GetArgs(args));
+
+        private static string[] GetArgs(string[] args)
+        {
+            launchArgs = args;
+            return args;
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
