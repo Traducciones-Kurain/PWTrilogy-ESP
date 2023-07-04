95cf959c36951c37a5419ed87c390e1e02833418
WorstAquaPlayer <tf2worth@hotmail.com>
2023-07-04 01:05:13 -0300

    Renombrar la build de Linux asi se diferencia un poco

--- build patcher linux.bat
+++ build patcher linux.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 cd AATrilogyPatcher
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --output "bin\Publish\net5.0-linux"
+ren bin\Publish\net5.0-linux\AATrilogyPatcher AATrilogyPatcher-linux
