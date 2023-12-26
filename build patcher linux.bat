4692627a01720f59c6314f9be21f3b8ff5a611a5
WorstAquaPlayer <maxwellaquaruiz@gmail.com>
2023-12-26 00:27:49 -0300

    Pequeño arreglo para el script de Linux

--- build patcher linux.bat
+++ build patcher linux.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 cd PWTrilogyPatcher.Desktop
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --output "bin\Publish\net7.0-linux"
-ren bin\Publish\net7.0-windows\PWTrilogyPatcher.Desktop AATrilogyPatcher-linux
+ren bin\Publish\net7.0-linux\PWTrilogyPatcher.Desktop AATrilogyPatcher-linux
